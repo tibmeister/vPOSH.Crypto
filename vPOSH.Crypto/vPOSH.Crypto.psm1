c0c1e88268dff0a0f40e55b3bffa77174ba389b9
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2021-05-06 16:17:03 -0500

    Added new function

--- vPOSH.Crypto/vPOSH.Crypto.psm1
+++ vPOSH.Crypto/vPOSH.Crypto.psm1
@@ -175,5 +175,112 @@ function Test-FileHash
 
 function New-RandomPassword
 {
+<#
+    .SYNOPSIS
+        Generate a random password
+    .DESCRIPTION
+        Generate a random password with some advanced functionality
+    .EXAMPLE
+        Get-RandomPassword.ps1 -ExcludedCharacters '@','I','l','!' -MinimumLength 10 -MaximumLength 42
+
+        Exclude the characters @,I,l, and ! from the generated password and set the minimum
+        length to 10 characters and maximum length to 42 characters long.
+    .EXAMPLE
+        Get-RandomPassword.ps1 -ExcludedCharacters '@','I','l','!','_','`' -NonDuplicating
+
+        Exclude the characters @,I,l, and ! from the generated password and do not reuse any characters in
+        the generated password
+#>
+[CmdletBinding()]
+param
+(
+    # Minimum length of generated password, 15 by default
+    [Parameter(Mandatory = $False)]
+    [int]$MinimumLength = 15,
+
+    # Maximum length of the generated password, 25 by default
+    [Parameter(Mandatory = $False)]
+    [int]$MaximumLength = 25,
+
+    # Array list of characters to exclude from the password
+    [Parameter(Mandatory = $False)]
+    [string[]]$ExcludedCharacters,
+
+    # Do not allow for duplicated characters
+    [Parameter(Mandatory=$False)]
+    [switch]$NonDuplicating
+)
+
+$LC_Letters = [int][char]'a'..[int][char]'z' |
+ForEach-Object { [char]$_ }
+$UC_Letters = [int][char]'A'..[int][char]'Z' |
+ForEach-Object { [char]$_ }
+
+[System.Collections.ArrayList]$Digits = 0..9
+[System.Collections.ArrayList]$SpecialChars = @(
+    '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
+    '_', '-', '+', '=', '{', '}', '[', ']', '\', '|', ':', ';'
+    '<', '>', ',', '.', '?', '/'
+)
+
+# Gather them all together into a new ArrayList
+[System.Collections.ArrayList]$FullCharSet = {$LC_Letters + $UC_Letters + $Digits + $SpecialChars}.Invoke()
+
+# If there's any Excluded Characters, remove them from the FullCharSet now
+if($ExcludedCharacters.Length -gt 0)
+{
+    foreach($exChar in $ExcludedCharacters)
+    {
+        $FullCharSet.Remove($exChar)
+    }
+}
+
+$PwdLength = (Get-Random -Minimum $MinimumLength -Maximum $MaximumLength)
+
+# Check to make sure if we are in NonDuplicating mode we have enough Characters in the FullCharSet
+if($NonDuplicating)
+{
+    if($PwdLength -gt $FullCharSet.Count)
+    {
+        Write-Error "Your chosen password is longer than the available character set and you've specified NonDuplicating mode"
+        exit 99
+    }
+}
+
+$Password = ''
+
+foreach ($Item in 1..$PwdLength)
+{
+    $IsDupe = $True
+    while ($IsDupe)
+    {
+        $NewChar = Get-Random -InputObject $FullCharSet -Count 1
+
+        # If we are in NonDuplicating mode, then remove the charactor from the FullCharSet
+        if($NonDuplicating)
+        {
+            $FullCharSet.Remove($NewChar)
+        }
+
+        if (($Password.Length -eq 0) -or
+            # test for 2-in-a-row
+            (($Password.Length -eq 1) -and ($Password -cne $NewChar)) -or
+            # test for 3-in-a-row
+            (($Password.Length -gt 1) -and ($Password[-1] -cne $NewChar) -and ($Password[-2] -cne $Password[-1]))
+        )
+        {
+            $Password += $NewChar
+            $IsDupe = $False
+        }
+        # else
+        # {
+        #     $Password
+        #     $NewChar
+        # }
+    }
+}
+
+return $Password
+
 
 }
